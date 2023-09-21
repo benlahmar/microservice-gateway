cd356da6be79e236e26ce0fd79d98f5beab67199
benlahmar habib <h.benlahmer@gmail.com>
2023-09-21 14:16:28 +0100

    add

---
+++ gatewayrma/src/main/java/com/example/demo/GatewayrmaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayrmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayrmaApplication.class, args);
+	}
+
+}
